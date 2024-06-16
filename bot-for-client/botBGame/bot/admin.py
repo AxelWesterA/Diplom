2457534dcdc89674b66d82ccd57c2cb996cec949
Александр Кретинин <alexanderwester7@gmail.com>
2024-06-16 19:45:32 +0400

    f1

--- bot-for-client/botBGame/bot/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# bot/admin.py
-from django.contrib import admin
-from .models import Admin, Client
-
-admin.site.register(Admin)
-admin.site.register(Client)
